01a1baef395412422cbe06ede1e6b1fb220bc4c6
Emmanouil Frantzeskos <manosfrantzeskos@gmail.com>
2022-10-02 10:19:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function loadLazyImage(entries, observer) {
 }
 
 
-const sectionsObserver = new IntersectionObserver(displaySection, {threshold: 0.3});
+const sectionsObserver = new IntersectionObserver(displaySection, {threshold: 0.45});
 
 mainSections.forEach(section => {
     sectionsObserver.observe(section)
